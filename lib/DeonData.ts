2146e2606bb889a93ae6c7dd032209b8fa7de488
Adam Schønemann <adamschoenemann@gmail.com>
2018-10-09 15:41:45 +0200

    support signed data

--- lib/DeonData.ts
+++ lib/DeonData.ts
@@ -1,4 +1,9 @@
 import { Duration, durationToISOString } from './ISO8601Duration';
+import * as Elliptic from 'elliptic';
+import { sha3_256 } from 'js-sha3';
+import * as lapo_asn1js from '@lapo/asn1js';
+import { unarmor } from '@lapo/asn1js/base64';
+import base64Js from 'base64-js';
 
 /* API Input and output wrappers */
 export interface DeclarationInput {
@@ -151,13 +156,30 @@ export const mkDurationValue = (duration: Duration): DurationValue =>
 export const mkInstantValue = (instant: Date): InstantValue =>
   ({ class: 'InstantValue', instant: instant.toISOString() });
 
+export enum CurveName { secp256k1 = 'secp256k1' }
+
 export interface ECDSAPublicKey {
   tag: 'ECDSAPublicKey';
   pem: string;
-  curveName: string;
+  curveName: CurveName;
 }
 
+// not used in CSL but for API purposes we create a proper datatype for it.
+// This is also why there is no PublicKeyValue
+export interface ECDSAPrivateKey {
+  tag: 'ECDSAPrivateKey';
+  pem: string;
+  curveName: CurveName;
+}
+
+export const mkECDSAPrivateKey = (pem: string): PrivateKey => ({
+  pem,
+  tag: 'ECDSAPrivateKey',
+  curveName: CurveName.secp256k1,
+});
+
 export type PublicKey = ECDSAPublicKey;
+export type PrivateKey = ECDSAPrivateKey;
 
 export interface PublicKeyValue {
   class: 'PublicKeyValue';
@@ -171,7 +193,7 @@ export const mkECDSAPublicKeyValue = (pem: string, boundName: QualifiedName): Pu
   publicKey: {
     pem,
     tag: 'ECDSAPublicKey',
-    curveName: 'secp192r1', // TODO: dont use constant
+    curveName: CurveName.secp256k1,
   },
 });
 
@@ -195,16 +217,77 @@ export interface SignedValue {
   boundName: QualifiedName;
 }
 
-function stringToBytes(str: string): number[] {
-  let bytes:number[] = [];
-  for (let i = 0; i < str.length; i = i + 1) {
-    const code = str.charCodeAt(i);
-    bytes = bytes.concat([code & 0xff, code / 256 >>> 0]);
+const globalEC = new Elliptic.ec(CurveName.secp256k1);
+
+function decodeASN1PublicPem(pem: string) {
+  /*
+  Informal ASN.1 Schema of openssl elliptic curve private keys (undocumented).
+  You can find it (or something like it) by running `openssl asn1parse -in [PUBLIC_KEY] -dump`
+  OpenSSLECPublicKey ::= SEQUENCE {
+    metadata    SEQUENCE {
+      publicKeyType     OBJECT IDENTIFIER 1.2.840.10045.2.1,
+      ellipticCurve     OBJECT IDENTIFIER 1.2.840.10045.3.1.7,
+    }
+    publicKey   BIT STRING
   }
-  return bytes;
+  */
+  const decoded = lapo_asn1js.decode(unarmor(pem));
+  if (!decoded.sub) {
+    throw `Unexpected ASN.1 decode result:\n ${decoded}`;
+  }
+  const publicKey = decoded.sub[1];
+
+  // hexdump the bitstring of the public key
+  const bits =
+    publicKey.stream.hexDump(publicKey.posContent(), publicKey.posEnd(), true).substring(2);
+  return Buffer.from(bits, 'hex');
 }
 
-export const mkECDSASignedValue = (message: string, boundName: QualifiedName): SignedValue => {
+function decodeASN1PrivatePem(pem: string) {
+  /*
+  ASN.1 Schema of openssl elliptic curve private keys (https://tools.ietf.org/html/rfc5915):
+    ECPrivateKey ::= SEQUENCE {
+      version        INTEGER { ecPrivkeyVer1(1) } (ecPrivkeyVer1),
+      privateKey     OCTET STRING,
+      parameters [0] ECParameters {{ NamedCurve }} OPTIONAL,
+      publicKey  [1] BIT STRING OPTIONAL
+    }
+  */
+  const decoded = lapo_asn1js.decode(unarmor(pem));
+  if (!decoded.sub) {
+    throw `Unexpected ASN.1 decode result:\n ${decoded}`;
+  }
+  const privateKey = decoded.sub[1];
+  const hexstr = privateKey.stream.hexDump(privateKey.posContent(), privateKey.posEnd(), true);
+  return Buffer.from(hexstr, 'hex');
+}
+
+export function decodePublicKey(pem: string, ec = globalEC) {
+  const buffer = decodeASN1PublicPem(pem);
+  return ec.keyFromPublic(buffer);
+}
+
+export function decodePrivateKey(pem: string, ec = globalEC) {
+  const buffer = decodeASN1PrivatePem(pem);
+  return ec.keyFromPrivate(buffer);
+}
+
+export function checkSignature(pubk: PublicKey, signed: Signed): boolean {
+  const pubkdec = decodePublicKey(pubk.pem);
+  const hashed = sha3_256(signed.message);
+  const der = base64Js.toByteArray(signed.sig.signature.bytes);
+  return pubkdec.verify(hashed, der as any);
+}
+
+export const mkECDSASignedValue = (
+  privk: PrivateKey,
+  message: string,
+  boundName: QualifiedName,
+): SignedValue => {
+  const key = decodePrivateKey(privk.pem);
+  const hashedMessage = sha3_256(message);
+  const signature = key.sign(hashedMessage);
+  const pem = base64Js.fromByteArray(signature.toDER());
   return {
     boundName,
     class: 'SignedValue',
@@ -212,7 +295,7 @@ export const mkECDSASignedValue = (message: string, boundName: QualifiedName): S
       message,
       sig: {
         signature: {
-          bytes: stringToBytes(message).join(''),
+          bytes: pem,
         },
         tag: 'ECDSASignature',
       },
--- test/DeonData.spec.ts
+++ test/DeonData.spec.ts
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
 import 'mocha';
-import { qual } from '../lib/DeonData';
+import {
+  qual,
+  mkECDSAPublicKeyValue,
+  mkECDSAPrivateKey,
+  mkECDSASignedValue,
+  checkSignature,
+} from '../lib/DeonData';
 
 describe('QualifiedName', () => {
   it('qualified named with no module', () => {
@@ -8,5 +14,48 @@ describe('QualifiedName', () => {
   });
   it('qualified name with module', () => {
     expect(qual('A::a')).to.deep.equal({ name: 'a', qualifier: ['A'] });
+  });
+});
+
+describe('Signed data', () => {
+  it('correctly verifies data signed with corresponding public and private keys', () => {
+    const publicPem = `
+-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEFDpOIaItaN2oAaz4bVVMbFSq2jhYbpvS
+JyFpzshkKrjg1Up82XtpOibzmfQTPF+h5iOq9dC/P+BqQwKkVUkU+A==
+-----END PUBLIC KEY-----`;
+
+    const privatePem = `
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIBLDGd9V/M3AgxCo+O+A6GDDIaIY1QQyYL9x969eioJToAcGBSuBBAAK
+oUQDQgAEFDpOIaItaN2oAaz4bVVMbFSq2jhYbpvSJyFpzshkKrjg1Up82XtpOibz
+mfQTPF+h5iOq9dC/P+BqQwKkVUkU+A==
+-----END EC PRIVATE KEY-----`;
+    const pubkVal = mkECDSAPublicKeyValue(publicPem, qual('publicKey'));
+    const privk = mkECDSAPrivateKey(privatePem);
+    const signedVal =
+      mkECDSASignedValue(privk, 'Help me Obi-Wan Kenobi, you\'re my only hope!', qual('signed'));
+    expect(checkSignature(pubkVal.publicKey, signedVal.signed)).to.be.true;
+  });
+
+  it('correctly rejects data signed with public and private keys that are not paired', () => {
+    const publicPem = `
+-----BEGIN PUBLIC KEY-----
+MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEFDpOIaItaN2oAaz4bVVMbFSq2jhYbpvS
+JyFpzshkKrjg1Up82XtpOibzmfQTPF+h5iOq9dC/P+BqQwKkVUkU+A==
+-----END PUBLIC KEY-----`;
+
+    const privatePem = `
+-----BEGIN EC PRIVATE KEY-----
+MHQCAQEEIGDnTVzZgLvGiri2wbLpzrAjK+FdE/Q8D9O7UO4DhroRoAcGBSuBBAAK
+oUQDQgAEdy9CBHRkqwhP4IfQFmj386JU1bB4R15fKVW8MmIObtREFJ4cYDWHo7Ju
+vSQCx5o2XUXD2t82qOY8J3/ByehWSQ==
+-----END EC PRIVATE KEY-----`;
+
+    const pubkVal = mkECDSAPublicKeyValue(publicPem, qual('publicKey'));
+    const privk = mkECDSAPrivateKey(privatePem);
+    const signedVal =
+      mkECDSASignedValue(privk, 'Help me Obi-Wan Kenobi, you\'re my only hope!', qual('signed'));
+    expect(checkSignature(pubkVal.publicKey, signedVal.signed)).to.be.false;
   });
 });
