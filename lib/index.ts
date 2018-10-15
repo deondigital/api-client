77747440fd7e261073ab73b983ba4bc2b8e47b06
Adam Schønemann <adamschoenemann@gmail.com>
2018-10-15 15:16:22 +0200

    export key types

--- lib/index.ts
+++ lib/index.ts
@@ -7,5 +7,6 @@ export * from './valueToJson';
 import * as ISO8601Duration from './ISO8601Duration';
 export * from './Signed';
 export * from './ECDSA';
+export * from './Keys';
 
 export { ISO8601Duration };
