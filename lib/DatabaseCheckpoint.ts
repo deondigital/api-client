d2e24c6a23e13e79d27c7aa7bb544a833c10618d
Bjørn Bugge Grathwohl <bjornbugge@users.noreply.github.com>
2019-05-14 13:27:57 +0200

    Remove commented-out stuff

--- lib/DatabaseCheckpoint.ts
+++ lib/DatabaseCheckpoint.ts
@@ -3,12 +3,7 @@ export namespace DatabaseCheckpoint {
   export interface Fresh {
     tag: 'Fresh';
   }
-//   export interface At {
-//     tag: 'At';
-//     index: number;
-//   };
 }
 
 export type DatabaseCheckpoint
   = DatabaseCheckpoint.Fresh;
-//   | DatabaseCheckpoint.At;
