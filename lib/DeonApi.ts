db549f9e3c621b58dff974fc4799b3d3945b8315
Bjørn Bugge Grathwohl <bjorn.bugge@deondigital.com>
2020-12-07 11:30:01 +0100

    `Resolve` is not part of the API anymore

--- lib/DeonApi.ts
+++ lib/DeonApi.ts
@@ -224,18 +224,11 @@ export const isBadRequest = (x: unknown): x is { errors: any[] } =>
 
 export type Frame =
   | Call
-  | Resolve
 ;
 
 export interface Call {
   tag: 'Call';
   contractName: D.QualifiedName;
-  location: NodeRange;
-}
-
-export interface Resolve {
-  tag: 'Resolve';
-  varName: D.QualifiedName;
   location: NodeRange;
 }
 
